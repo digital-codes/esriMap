58d49d2dc531cedeff927c0fc5f5282865425f6c
Andreas Kugel <andreas.kugel@web.de>
2024-11-25 21:56:52 +0100

    plz areas

--- esri.js
+++ esri.js
@@ -18,9 +18,12 @@ import PictureMarkerSymbol from "@arcgis/core/symbols/PictureMarkerSymbol";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 
 import Polyline from "@arcgis/core/geometry/Polyline";
-import { SimpleLineSymbol } from "@arcgis/core/symbols";
+import { SimpleLineSymbol, SimpleFillSymbol } from "@arcgis/core/symbols";
 
 import PopupTemplate from "@arcgis/core/PopupTemplate";
+
+const withGraphics = false;
+
 
 /*
 import Point from "@arcgis/core/geometry/Point";
@@ -60,7 +63,13 @@ console.log("esriConfig.assetsPath", esriConfig.assetsPath);
 //import "@esri/calcite-components/dist/components/calcite-scrim";
 
 const vectorUrlBase = "https://basemaps.arcgis.com/arcgis/rest/services/World_Basemap_v2/VectorTileServer"
-const vectorUrl = "https://geoportal.karlsruhe.de/server/rest/services/Hosted/Regiokarte_farbig_Vektor/VectorTileServer"
+//const vectorUrl = "https://geoportal.karlsruhe.de/server/rest/services/Hosted/Regiokarte_farbig_Vektor/VectorTileServer"
+
+// regiokarte_farbig_reduziert
+const vectorUrl = "https://geoportal.karlsruhe.de/ags04/rest/services/Hosted/Regiokarte_farbig_reduziert/VectorTileServer"
+//const vectorUrlBase = "/tiles/base"
+//const vectorUrl = "/tiles/ka"
+
 
 // low zoom level
 //	https://geoportal.karlsruhe.de/server/rest/services/Hosted/Regiokarte_farbig_Vektor/VectorTileServer/tile/11/703/1071.pbf
@@ -102,124 +111,125 @@ export async function setupMap(element) {
   map.add(tileLayer);
 
   // Create a GraphicsLayer to hold markers
-  var graphicsLayer = new GraphicsLayer();
-  map.add(graphicsLayer);
+  if (withGraphics) {
+    var graphicsLayer = new GraphicsLayer();
+    map.add(graphicsLayer);
 
-  // Define marker symbols and popups
-  const addMarker = async (longitude, latitude, title, content) => {
-    var point = {
-      type: "point", // autocasts as new Point()
-      longitude: longitude,
-      latitude: latitude
-    };
-    /*
-    var markerSymbol = {
-        type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
-        color: [226, 119, 40],  // Orange
-        outline: {
-            color: [255, 255, 255],
-            width: 2
+    // Define marker symbols and popups
+    const addMarker = async (longitude, latitude, title, content) => {
+      var point = {
+        type: "point", // autocasts as new Point()
+        longitude: longitude,
+        latitude: latitude
+      };
+      /*
+      var markerSymbol = {
+          type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
+          color: [226, 119, 40],  // Orange
+          outline: {
+              color: [255, 255, 255],
+              width: 2
+          }
+      };
+      */
+      /*
+       var markerSymbol = new PictureMarkerSymbol({
+         url: "assets/custom/icons/github-mark.svg", // Path to your custom icon
+         width: "32px", // Adjust size as needed
+         height: "32px",
+         // Optionally, set an anchor point to position the icon correctly
+         yoffset: 16 // Moves the icon up by half its height
+       });
+       */
+      var markerSymbol = new PictureMarkerSymbol({
+        url: "assets/custom/icons/marker-icon-2x.png",  // Path to your custom icon
+        width: "32px", // Adjust size as needed
+        height: "50px",
+        // Optionally, set an anchor point to position the icon correctly
+        yoffset: 16 // Moves the icon up by half its height
+      });
+
+      var pointGraphic = new Graphic({
+        geometry: point,
+        symbol: markerSymbol,
+        popupTemplate: { // autocasts as new PopupTemplate()
+          title: title,
+          content: content
         }
-    };
-    */
-    /*
-     var markerSymbol = new PictureMarkerSymbol({
-       url: "assets/custom/icons/github-mark.svg", // Path to your custom icon
-       width: "32px", // Adjust size as needed
-       height: "32px",
-       // Optionally, set an anchor point to position the icon correctly
-       yoffset: 16 // Moves the icon up by half its height
-     });
-     */
-    var markerSymbol = new PictureMarkerSymbol({
-      url: "assets/custom/icons/marker-icon-2x.png",  // Path to your custom icon
-      width: "32px", // Adjust size as needed
-      height: "50px",
-      // Optionally, set an anchor point to position the icon correctly
-      yoffset: 16 // Moves the icon up by half its height
-    });
+      });
 
-    var pointGraphic = new Graphic({
-      geometry: point,
-      symbol: markerSymbol,
-      popupTemplate: { // autocasts as new PopupTemplate()
-        title: title,
-        content: content
-      }
-    });
-
-    graphicsLayer.add(pointGraphic);
-  }
-
-  // img not working without full http path
-
-  points.forEach(point => {
-    let content = point.NAME;
-    if (point.IMGURL && point.IMGURL.length > 0) {
-      if (point.IMGURL.startsWith("http")) {
-        content = `<div class='popup-content'><h4>${point.NAME}</h4><img src="${point.IMGURL}" alt='Supported' style='width: 100px;'><p>xxx</p></div>`
-      } else {
-        content = `<div class='popup-content'><h4>${point.NAME}</h4><img src="${assetPathUrl}/${point.IMGURL}" alt='Supported' style='width: 100px;'><p>xxx</p></div>`
-      }
+      graphicsLayer.add(pointGraphic);
     }
-    addMarker(point.LAT, point.LON, point.OBJECTID, content);
-  });
+    // img not working without full http path
 
-  // add points from fetched data
-  let dynamicPoints = [];
-  const req = await fetch("/data/weatherPois.json")
-  if (req.ok) {
-    dynamicPoints = await req.json();
-    console.log("dynamicPoints", dynamicPoints.features);
-    dynamicPoints.features.forEach(point => {
-      const coords = point.geometry.coordinates;
-      const props = point.properties;
-      let content = props.name;
-      if (props.img && props.img.length > 0) {
-        if (props.img.startsWith("http")) {
-          content = `<div class='popup-content'><h4>${props.name}</h4><img src="${props.img}" alt='Supported' style='width: 100px;'><p>${props.attribution}</p></div>`
+    points.forEach(point => {
+      let content = point.NAME;
+      if (point.IMGURL && point.IMGURL.length > 0) {
+        if (point.IMGURL.startsWith("http")) {
+          content = `<div class='popup-content'><h4>${point.NAME}</h4><img src="${point.IMGURL}" alt='Supported' style='width: 100px;'><p>xxx</p></div>`
         } else {
-          content = `<div class='popup-content'><h4>${props.name}</h4><img src="${assetPathUrl}/${props.img}" alt='Supported' style='width: 100px;'><p></p></div>`
+          content = `<div class='popup-content'><h4>${point.NAME}</h4><img src="${assetPathUrl}/${point.IMGURL}" alt='Supported' style='width: 100px;'><p>xxx</p></div>`
         }
       }
-      addMarker(coords[0], coords[1], null, content);
+      addMarker(point.LAT, point.LON, point.OBJECTID, content);
     });
 
-  }
+    // add points from fetched data
+    let dynamicPoints = [];
+    let req
+    req = await fetch("/data/weatherPois.json")
+    if (req.ok) {
+      dynamicPoints = await req.json();
+      console.log("dynamicPoints", dynamicPoints.features);
+      dynamicPoints.features.forEach(point => {
+        const coords = point.geometry.coordinates;
+        const props = point.properties;
+        let content = props.name;
+        if (props.img && props.img.length > 0) {
+          if (props.img.startsWith("http")) {
+            content = `<div class='popup-content'><h4>${props.name}</h4><img src="${props.img}" alt='Supported' style='width: 100px;'><p>${props.attribution}</p></div>`
+          } else {
+            content = `<div class='popup-content'><h4>${props.name}</h4><img src="${assetPathUrl}/${props.img}" alt='Supported' style='width: 100px;'><p></p></div>`
+          }
+        }
+        addMarker(coords[0], coords[1], null, content);
+      });
 
-  /*
-  //const mk1Pop = "<div class='popup-content'><h4>This is the very first marker.</h4><img src='/assets/custom/images/support.png' alt='Supported' style='width: 100px;'><p>xxx</p></div>"
-  //const imgUrl = assetPathUrl + "/custom/images/support.png";
-  // const mk1Pop = `<div class='popup-content'><h4>This is the very first marker.</h4><img src="${imgUrl}" alt='Supported' style='width: 100px;'><p>xxx</p></div>`
-  // Add sample markers
-  const mk1 = addMarker(8.4, 49.01, "Marker 1", mk1Pop);
-  const mk2 = addMarker(8.405, 49.015, "Marker 2", "This is the second marker.");
-  const mk3 = addMarker(8.395, 49.005, "Marker 3", "This is the third marker.");
-  console.log("Markers added", mk1, mk2, mk3);
-  */
-
-  // polygon
-  // ------------
-  // Create a polygon geometry
-  const polygon = {
-    type: "polygon",
-    rings: [
-      [8.42, 49.011], //Longitude, latitude
-      [8.44, 49.013], //Longitude, latitude
-      [8.45, 49.016], //Longitude, latitude
-      [8.43, 49.014], //Longitude, latitude
-      [8.41, 49.012], //Longitude, latitude
-    ]
-  };
-
-  const simpleFillSymbol = {
-    type: "simple-fill",
-    color: [227, 139, 79, 0.8], // Orange, opacity 80%
-    outline: {
-      color: [255, 255, 255],
-      width: 1
     }
-  };
+
+    /*
+    //const mk1Pop = "<div class='popup-content'><h4>This is the very first marker.</h4><img src='/assets/custom/images/support.png' alt='Supported' style='width: 100px;'><p>xxx</p></div>"
+    //const imgUrl = assetPathUrl + "/custom/images/support.png";
+    // const mk1Pop = `<div class='popup-content'><h4>This is the very first marker.</h4><img src="${imgUrl}" alt='Supported' style='width: 100px;'><p>xxx</p></div>`
+    // Add sample markers
+    const mk1 = addMarker(8.4, 49.01, "Marker 1", mk1Pop);
+    const mk2 = addMarker(8.405, 49.015, "Marker 2", "This is the second marker.");
+    const mk3 = addMarker(8.395, 49.005, "Marker 3", "This is the third marker.");
+    console.log("Markers added", mk1, mk2, mk3);
+    */
+
+    // polygon
+    // ------------
+    // Create a polygon geometry
+    const polygon = {
+      type: "polygon",
+      rings: [
+        [8.42, 49.011], //Longitude, latitude
+        [8.44, 49.013], //Longitude, latitude
+        [8.45, 49.016], //Longitude, latitude
+        [8.43, 49.014], //Longitude, latitude
+        [8.41, 49.012], //Longitude, latitude
+      ]
+    };
+
+    const simpleFillSymbol = {
+      type: "simple-fill",
+      color: [227, 139, 79, 0.8], // Orange, opacity 80%
+      outline: {
+        color: [255, 255, 255],
+        width: 1
+      }
+    };
     const polyPop = {
       title: "{Name}",
       content: "{Description}"
@@ -229,57 +239,57 @@ export async function setupMap(element) {
       Description: "I am a polygon"
     };
 
-  const polygonGraphic = new Graphic({
-    geometry: polygon,
-    symbol: simpleFillSymbol,
-    attributes: attributes,
-    popupTemplate: polyPop
-  });
-  graphicsLayer.add(polygonGraphic);
+    const polygonGraphic = new Graphic({
+      geometry: polygon,
+      symbol: simpleFillSymbol,
+      attributes: attributes,
+      popupTemplate: polyPop
+    });
+    graphicsLayer.add(polygonGraphic);
 
-  // dynamically remove items from graphics layer
-  let symTgl = false
-  // make the polygon fill toggle
-  const tglFill = () => {
-    console.log("toggle",symTgl)
-    const color = symTgl ?  [227, 139, 79, 0.8] :  [20,150, 227, 0.8]
-    polygonGraphic.symbol = 
-    {
-      type: "simple-fill",
-      color: color,
-      outline: {
-        color: [255, 255, 255], // white outline
-        width: 2
+    // dynamically remove items from graphics layer
+    let symTgl = false
+    // make the polygon fill toggle
+    const tglFill = () => {
+      console.log("toggle", symTgl)
+      const color = symTgl ? [227, 139, 79, 0.8] : [20, 150, 227, 0.8]
+      polygonGraphic.symbol =
+      {
+        type: "simple-fill",
+        color: color,
+        outline: {
+          color: [255, 255, 255], // white outline
+          width: 2
+        }
       }
+      symTgl = !symTgl
+      //
+      const items = graphicsLayer.graphics.length
+      console.log("Items:", items)
+      for (let i = 0; i < items; i++) {
+        console.log("item:", graphicsLayer.graphics.getItemAt(i))
+      }
+      if (items > 1) {
+        const g = graphicsLayer.graphics.getItemAt(items - 1)
+        graphicsLayer.remove(g)
+      }
+      //
+      setTimeout(tglFill, 2000)
     }
-    symTgl = !symTgl
-    //
-    const items = graphicsLayer.graphics.length
-    console.log("Items:",items)
-    for (let i = 0;i < items; i++) {
-      console.log("item:",graphicsLayer.graphics.getItemAt(i))
-    }
-    if (items > 1) {
-      const g = graphicsLayer.graphics.getItemAt(items - 1)
-      graphicsLayer.remove(g)
-    }
-    //
-    setTimeout(tglFill,2000)
-  }
-  // larger delay on first call
-  setTimeout(tglFill,8000)
+    // larger delay on first call
+    setTimeout(tglFill, 8000)
 
-  // tracks
-  const popupTracks = new PopupTemplate({
-    title: "Trail Name: {TRL_NAME}",
-    content: `
+    // tracks
+    const popupTracks = new PopupTemplate({
+      title: "Trail Name: {TRL_NAME}",
+      content: `
         <div class='popup-content'>
           <p><strong>Trail ID:</strong> {TRL_ID}</p>
           <p><strong>Property:</strong> {PROP}</p>
           <p><strong>Usage:</strong> {USE}</p>
         </div>
       `
-  });
+    });
 
   /**
    * Converts an array of GeoJSON-like features into Esri Graphic objects.
@@ -306,6 +316,7 @@ export async function setupMap(element) {
         paths: [coordinates], // Esri expects an array of paths
         spatialReference: { wkid: 4326 } // WGS84
       });
+      console.log("track coords", coordinates);
 
       // Define the symbol for the track line
       const symbol = new SimpleLineSymbol({
@@ -336,7 +347,7 @@ export async function setupMap(element) {
         },
         // popups in feature layer
       });
-
+      console.log("track graphic", graphic);
       return graphic;
     }).filter(graphic => graphic !== null); // Remove any null entries due to unsupported geometries
   }
@@ -376,6 +387,127 @@ export async function setupMap(element) {
 
   // Add the FeatureLayer to the map
   map.add(trackLayer);
+}
 
+
+  // plz layer
+  const req2 = await fetch("/data/plz.geojson")
+  if (req2.ok) {
+    const plzGraphics = await req2.json();
+    console.log("plz geojson features", plzGraphics.features);
+    // Convert GeoJSON Features to FeatureLayer Source
+    const plzFeatures = plzGraphics.features.map((feature, index) => {
+      return {
+        geometry: {
+          type: feature.geometry.type.toLowerCase(), // e.g., "polygon"
+          rings: feature.geometry.coordinates[0], // Coordinates for polygon
+        },
+        attributes: {
+          ...feature.properties,
+          OBJECTID: index + 1, // Generate OBJECTID for Esri FeatureLayer
+        },
+      };
+    });
+
+    // Define the fields for the FeatureLayer
+    const plzFields = [
+      {
+        name: "OBJECTID",
+        alias: "Object ID",
+        type: "oid",
+      },
+      {
+        name: "PLZ",
+        alias: "Postal Code",
+        type: "string",
+      },
+      {
+        name: "SHAPE_Area",
+        alias: "Shape Area",
+        type: "double",
+      },
+    ];
+
+    // Define color mapping for the PLZs
+    const colorMap = {
+      "76131": [255, 0, 0, 0.1],    // Red
+      "76133": [0, 255, 0, 0.1],    // Green
+      "76135": [0, 0, 255, 0.1],    // Blue
+      "76137": [255, 255, 0, 0.1],  // Yellow
+      "76139": [255, 0, 255, 0.1],  // Magenta
+      "76141": [0, 255, 255, 0.1],  // Cyan
+      "76149": [128, 0, 128, 0.1],  // Purple
+      "76187": [255, 165, 0, 0.1],  // Orange
+      "76199": [0, 128, 128, 0.1],  // Teal
+      "76227": [128, 128, 0, 0.1],  // Olive
+      "76228": [128, 0, 0, 0.1],    // Maroon
+      "76229": [0, 128, 0, 0.1],    // Dark Green
+      "76275": [0, 0, 128, 0.1],    // Navy
+      "76297": [192, 192, 192, 0.1], // Silver
+      "76307": [105, 105, 105, 0.1], // Dim Gray
+      "76185": [105, 70, 105, 0.1], // 
+      "76189": [60, 70, 135, 0.1], // 
+    };
+
+    // Create unique value renderer
+    const uniqueValueInfos = Object.keys(colorMap).map((plz) => ({
+      value: plz,
+      symbol: {
+        type: "simple-fill",
+        color: colorMap[plz], // Color from the color map
+        outline: {
+          color: [0, 0, 0], // White outline
+          width: 2,
+        },
+      },
+      label: `PLZ: ${plz}`,
+    }));
+
+    const renderer = {
+      type: "unique-value",
+      field: "PLZ", // Field to match
+      uniqueValueInfos: uniqueValueInfos,
+      defaultSymbol: {
+        type: "simple-fill",
+        color: [200, 200, 200, 0.4], // Default gray for unmatched
+        outline: {
+          color: [255, 255, 255],
+          width: 1,
+        },
+      },
+      defaultLabel: "Other PLZs",
+    };
+
+    // Create the FeatureLayer
+    const plzFeatureLayer = new FeatureLayer({
+      source: plzFeatures, // GeoJSON features as Esri Graphics
+      fields: plzFields,
+      objectIdField: "OBJECTID", // Required for Esri FeatureLayer
+      geometryType: "polygon", // Geometry type
+      renderer: renderer, // Unique value renderer
+      /*
+      renderer: {
+        type: "simple", // Simple renderer
+        symbol: {
+          type: "simple-fill",
+          color: [0, 0, 0, 0.2], // Fill color with transparency
+          outline: {
+            color: [255, 255, 255],
+            width: 1,
+          },
+        },
+      },
+      */
+      popupTemplate: {
+        title: "PLZ: {PLZ}",
+        content: `
+                        <ul>
+                            <li><b>Fläche:</b> {SHAPE_Area} m²</li>
+                        </ul>
+                    `,
+      },
+    });
+    map.add(plzFeatureLayer);
+  }
 
 }
